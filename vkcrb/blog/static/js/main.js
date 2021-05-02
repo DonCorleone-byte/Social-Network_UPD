0ec800b426c6a2ec7d039be97c75b9089839100e
DonCorleone-byte <maks_umov1996@mail.ru>
2021-05-02 12:50:01 +0600

    blog

---
+++ vkcrb/blog/static/js/main.js
@@ -0,0 +1,12 @@
+$(function(){  
+
+     $('.dashboard__slider-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+    });
+
+});
